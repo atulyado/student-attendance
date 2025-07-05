88590e741ed5a6348af047d56371b4b59cd9f632
atulyado <133553524+atulyado@users.noreply.github.com>
2025-07-05 22:00:50 +0700

    Update studentAttendance.java

--- studentAttendance.java
+++ studentAttendance.java
@@ -5,19 +5,19 @@ class Solution {
          int m = s.length();
         for(int i=0;i<m;i++){
             int c = s.charAt(i);
-            if(c == 'A'){
+            if(c == 'A'){//check if there how many absent there are if more than two it will return false
                 absent++;
                 if(absent >= 2){
                     return false;
                 }
             }
-            if(c == 'L'){
+            if(c == 'L'){// check how many late there are if more than 3 it return false
                 latestreak++;
                 if(latestreak >= 3){
                     return false;
                 }
                 }else{
-                    latestreak = 0;
+                    latestreak = 0;//and here it reset the streak cause we need Three consecutive days
             }
         }
         return true;
